bf10a6f8497cfb97a85bd7d0c192ceba3d3573fb
Andrea Centeno <andreacent8@gmail.com>
2016-12-12 05:18:33 -0400

    luces, colisiones

---
+++ Assets/code/Colisiones.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Colisiones : MonoBehaviour {
+
+	void OnTriggerEnter (Collider col) 
+    {
+		if (col.CompareTag ("moneda")) {
+			Destroy (col.gameObject);
+		}
+		if (col.CompareTag ("moneda1")) {
+			Destroy (col.gameObject);
+		}
+		if (col.CompareTag ("moneda2")) {
+			Destroy (col.gameObject);
+		}
+		if (col.CompareTag ("moneda3")) {
+			Destroy (col.gameObject);
+		}
+		if (col.CompareTag ("moneda4")) {
+			Destroy (col.gameObject);
+		}
+		if (col.CompareTag ("moneda5")) {
+			Destroy (col.gameObject);
+		}
+
+	}
+}
